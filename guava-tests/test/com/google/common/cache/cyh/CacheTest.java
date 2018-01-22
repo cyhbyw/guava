d0f5d09bdac3a689767c9e929dbdb9cfc926ce5c
cyhbyw <swust2009cyh@qq.com>
2018-01-22 17:17:11 +0800

    Enhance CYH Cache test to  cover more impl code

--- guava-tests/test/com/google/common/cache/cyh/CacheTest.java
+++ guava-tests/test/com/google/common/cache/cyh/CacheTest.java
@@ -16,19 +16,27 @@ public class CacheTest {
     private Cache<Integer, Double> cache = CacheBuilder.newBuilder().maximumSize(1000).build();
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new CacheTest().run();
     }
 
-    private void run() {
-        int threadCount = 5;
+    private void run() throws InterruptedException {
+        // 第一次模板多线程的并发操作：读 + 没有值就创建值并放入缓存
+        multiThreadGet(1);
+        TimeUnit.SECONDS.sleep(20);
+        // 第二次，值已存在于缓存中，则直接读
+        multiThreadGet(2);
+    }
+
+    private void multiThreadGet(final int x) {
+        int threadCount = 3;
         while (threadCount-- > 0) {
             new Thread(() -> {
-                int key = 2;
-                while (key-- > 0) {
-                    System.out.println(Thread.currentThread().getName() + " --> " + retrieveValue(key));
+                int key = 2 * x;
+                for (int i = 0; i < key; i++) {
+                    System.out.println(Thread.currentThread().getName() + " --> " + retrieveValue(i));
                 }
-            }, "thread_" + threadCount).start();
+            }, "thread_" + x + "_" + threadCount).start();
         }
     }
 
@@ -39,7 +47,7 @@ public class CacheTest {
                 @Override
                 public Double call() throws InterruptedException {
                     System.out.println(Thread.currentThread().getName() + " really obtain key: " + key);
-                    TimeUnit.SECONDS.sleep(2);
+                    TimeUnit.SECONDS.sleep(1);
                     return Math.random();
                 }
             });
